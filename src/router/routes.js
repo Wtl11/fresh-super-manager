2b8b1896e0e2cdca24c5dca6d64ac7a87c519332
zhouxuan <zhouxuan@jkweixin.com>
2019-04-11 17:16:45 +0800

    去除结算周期

--- src/router/routes.js
+++ src/router/routes.js
@@ -59,6 +59,7 @@ export default [
         component: () => lazyLoadView(import('@pages/edit-franchise/edit-franchise')),
         meta: {
           titles: ['客户', '加盟商', '新建加盟商'],
+          variableIndex: 3,
           beforeResolve(routeTo, routeFrom, next) {
             if (!routeTo.query.id) {
               return next()
