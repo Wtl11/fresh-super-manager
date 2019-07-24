d2e2d6a923dfcc47b4c85a578177be8ee193a40c
刘美 <liumei@jkweixin.net>
2019-07-24 18:16:24 +0800

    去除首图上传 视频功能 ，只能上传图片

--- src/pages/article-add/article-add.vue
+++ src/pages/article-add/article-add.vue
@@ -61,25 +61,25 @@
             {{name}}封面
           </div>
           <div class="edit-input-box flex-box">
-            <!--currentType!=='video' ?'image-video' :'-->
-            <base-upload :videoUrl="addData.coverVideo.url"
-                         :imageUrl="addData.coverImage.url"
-                         :videoSize="10"
+            <!--currentType!=='video' ?'image-video' :'
+             :videoUrl="addData.coverVideo.url"
+              :videoSize="10"
+              @successVideo="getCoverVideo"-->
+            <base-upload :imageUrl="addData.coverImage.url"
                          :disabled="isDisabled"
                          fileType="image"
                          @failFile="failFile"
-                         @getPic="getPic"
-                         @delPic="delPic"
-                         @successVideo="getCoverVideo"
+                         @getPic="getCoverImage"
+                         @delPic="delCoverImage"
             ></base-upload>
             <div v-if="!isDisabled" class="tip">
               <template v-if="currentType === 'video'">
                 请添加不大于10M的清晰图片
               </template>
               <template v-else>
-                请添加不大于10M的清晰图片
+                请添加不大于10M的清晰图片,
                 <!--请添加不大于10M的清晰图片或视频(格式:mp4、3gp、m3u8、webm)-->
-                <br>
+                <!--<br>-->
                 {{name}}封面是{{name}}首图
               </template>
             </div>
@@ -514,38 +514,31 @@
         })
       },
       // 封面
-      getCoverVideo(video) {
-        this.addData.coverVideo.id = video.id
-        this.addData.coverVideo.file_id = video.file_id
-        this.addData.coverVideo.url = video.full_url
-        this.addData.coverImage.id = video.cover_image_id
-        this.addData.coverImage.url = video.full_cover_url
+      // getCoverVideo(video) {
+      //   this.addData.coverVideo.id = video.id
+      //   this.addData.coverVideo.file_id = video.file_id
+      //   this.addData.coverVideo.url = video.full_url
+      //   this.addData.coverImage.id = video.cover_image_id
+      //   this.addData.coverImage.url = video.full_cover_url
         // if (!this.addData.coverImage.id) {
         //   setTimeout(() => {
         //     this._getCoverImage(this.addData.coverVideo.file_id)
         //   }, 10000)
         // }
-      },
-      _getCoverImage(file_id) {
-        this.addData.coverVideo.file_id && API.Content.getCoverImage({file_id}).then(res => {
-          if (res.error !== this.$ERR_OK) return false
-          this.addData.coverImage.id = res.data.cover_image_id
-          this.addData.coverImage.url = res.data.full_cover_url
-        })
-      },
-      getPic(image) {
+      // },
+      getCoverImage(image) {
         this.addData.coverImage.url = image.url
         this.addData.coverImage.id = image.id
       },
-      delPic() {
+      delCoverImage() {
         this.addData.coverImage = {
           url: '',
           id: ''
         }
-        this.addData.coverVideo = {
-          url: '',
-          id: ''
-        }
+        // this.addData.coverVideo = {
+        //   url: '',
+        //   id: ''
+        // }
       },
       failFile(msg) {
         this.$toast.show(msg)
@@ -615,11 +608,8 @@
         if (!this.addData.category) message = '请选择内容分类'
         else if (!this.addData.title) message = '请输入文章标题'
         else if (this.addData.title && (this.addData.title.length < 5 || this.addData.title.length > 50)) message = '请输入文章标题最少5个最多50个字符'
-        else if (!this.addData.coverVideo.id && !this.addData.coverImage.id) message = '请上传封面'
-        // else if (this.currentType !== 'video' && this.addData.coverVideo.id && !this.addData.coverImage.id) {
-        //   this._getCoverImage(this.addData.coverVideo.file_id)
-        //   message = '正在处理视频第一帧作为封面图，请稍等5s上线'
-        // }
+        //  !this.addData.coverVideo.id &&
+        else if (!this.addData.coverImage.id) message = '请上传封面'
         else if (!this.addData.authPhoto.id) message = '请上传作者头像'
         else if (!this.addData.authName) message = '请填写作者名字'
         else if (!this.addData.authSignature) message = '请填写作者签名'
